0d0ed304b108eb4bcade2ff62824cc6bf16820b8
Murat Alkan <muratalkanw@hotmail.com>
2022-03-11 11:29:30 +0300

    Update to new data generator

--- utils/data_generator.py
+++ utils/data_generator.py
@@ -6,40 +6,64 @@ import numpy as np
  
 class DataGenerator:
     
-    def __init__(self, value, value_type='RN'):
+    def __init__(self, key, value, value_type='RN'):
+        self.key = key
         self.value = value
         self.value_type = value_type
 
-    def generateData(self):
+    def get_key(self):
+        return self.key
+
+    def generate_data(self):
         newValue = lambda generated_value: self.value + generated_value
-        self.value = newValue(self.get_random_value())
+        self.value = newValue( self.get_value() )
         return self.value
 
-    def get_random_value(self):
-        values = np.random.choice([-1, 0, 1], 1, p=[0.075, 0.85, 0.075]) #total p => 1
-        return int(values[0]) 
+    def get_value(self):
+        if self.value_type == 'RN': #RandomNumber
+            return self.generate_random_value()
+        elif self.value_type == 'RFN': #RandomFloatNumber
+            return self.generate_random_float_value()
+        elif self.value_type == 'RFN-2': #RandomFloatNumber
+            return self.generate_random_float_value2()
+        else: #ConstantNumber
+            return self.generate_constant()
+
+    def generate_random_value(self):
+        return int( 
+            self.generate([-1, 0, 1]) 
+            )
+        
+    def generate_random_float_value(self):
+        return float("{0:.2f}".format( 
+            self.generate([-0.1, 0, 0.1]) 
+            ))
+
+    def generate_random_float_value2(self):
+        return float("{0:.2f}".format( 
+            self.generate([-0.5, 0, 0.5]) 
+            ))
+
+    def generate_random_float_value3(self):
+        return float("{0:.3f}".format( 
+            self.generate([-0.05, 0, 0.05]) 
+            ))
+
+    def generate_random_float_value4(self):
+        return float("{0:.3f}".format( 
+            self.generate([-0.01, 0, 0.01]) 
+            ))
+
+    def generate_constant(self):
+        return self.value
+
+    def generate(self, valueList, p=[0.05, 0.9, 0.05]):
+        return np.random.choice(valueList, 1, p)[0]
         #numpy.random.choice(numpy.arange(1, 7), p=[0.1, 0.05, 0.05, 0.2, 0.4, 0.2])
-
-    def get_random_float_value(self):
-        values = np.random.choice([-0.1, 0, 0.1], 1, p=[0.075, 0.85, 0.075])
-        return int(values[0]) 
-
-    def get_random_float_value2(self):
-        values = np.random.choice([-0.5, 0, 0.5], 1, p=[0.075, 0.85, 0.075])
-        return int(values[0]) 
-
-    def get_constant(self):
-        return self.value
 
     #def get_custom_value(self):
 
-    '''
-    def convert(self):
-        if(self.value_type == 'RN'): #RandomNumber
 
-        elif(self.value_type == 'RFN'): #RandomFloatNumber
+    #def get_prob():
 
-        elif(self.value_type == 'RFN-2'): #RandomFloatNumber
-
-        else(self.value_type == 'CN'): #ConstantNumber
-    '''
+    #def set_prob():

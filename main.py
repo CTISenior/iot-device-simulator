ea4be4a9ac34730c933f05afd3b66c838c752600
Murat Alkan <muratalkanw@hotmail.com>
2022-03-11 11:30:15 +0300

    Update MainWindow

--- main.py
+++ main.py
@@ -1,47 +1,44 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
 import sys
-import json
+import os
 import logging
+import time
+import threading
 
-from windows.AddWindow import AddWindow
+from windows.AddDialog import AddDialog
 from connectors.client import Client
 import utils.helper as Helper
-
+import utils.gui_helper as GUIHelper
 
 from PyQt5.QtWidgets import (
-    QMainWindow, 
-    QHeaderView, 
-    QApplication, 
-    QScrollArea, 
-    QGridLayout, 
-    QListWidget, 
-    QMessageBox, 
-    QListWidgetItem, 
-    QGroupBox, 
-    QScrollBar, 
-    QTableWidget, 
-    QPushButton, 
-    QComboBox, 
-    QMenu, 
-    QWidget, 
-    QLabel, 
-    QLineEdit, 
-    QCheckBox, 
-    QAction, 
-    QTabWidget, 
-    QTableWidget, 
-    QTableWidgetItem, 
-    QSpinBox,
-    QVBoxLayout
+    QMainWindow,
+    QHeaderView,
+    QApplication,
+    QGridLayout,
+    QMessageBox,
+    QGroupBox,
+    QLabel,
+    QTableWidget,
+    QPushButton,
+    QMenu,
+    QWidget,
+    QAction,
+    QTableWidget,
+    QTableWidgetItem,
+    QVBoxLayout,
+    QAbstractItemView,
+    QPlainTextEdit
 )
 from PyQt5.QtCore import (
     Qt,
-    QSize, 
-    QEvent,
-    pyqtSlot
+    QEvent
 )
-from PyQt5.QtGui import QIcon
+from PyQt5.QtGui import (
+    QTextCursor
+)
+
+
 
 
 class MainWindow(QMainWindow):
@@ -53,20 +50,23 @@ class MainWindow(QMainWindow):
         self.height = 600
 
         self.setGeometry(self.left, self.top, self.width, self.height)
-        #self.setFixedSize(QSize(self.width, self.height))
         
-        self.setWindowTitle("IoT Device Simulator")
+        self.setWindowTitle("IoTwin | Data Generator")
         self.device_instance_list = [] 
 
+        Helper.init()
+
+        
+        self.logger = Helper.create_logger('main', './logs/main.log')
+        self.logger.debug('Main window created')
+
+        
         self.initMenuBar()
         self.initUI()
-        
-        self.add_window = AddWindow(self)
 
-        #logging.basicConfig(filename="logs/"+self.clientID, level=logging.DEBUG)
-        
-        logging.debug("Main Window")
-        
+        self.display_log_thread = None
+
+        self.add_window = AddDialog(self)
 
     def initMenuBar(self):
         menubar = self.menuBar()
@@ -85,8 +85,7 @@ class MainWindow(QMainWindow):
         newDevice_act.triggered.connect(self.addDeviceBtn)
         devicesMenuBar.addAction(newDevice_act)
         devicesMenuBar.addAction(command_act)
-        
-        
+
         helpMenuBar = menubar.addMenu('Help')
         thread_act = QAction('List Threads', self)
         helpMenuBar.addAction(thread_act)
@@ -121,9 +120,10 @@ class MainWindow(QMainWindow):
         vbox = QVBoxLayout()
 
         self.tableWidget = QTableWidget()
-        self.tableWidget.setColumnCount(6)
+        self.tableWidget.setColumnCount(8)
+        self.tableWidget.setColumnHidden(7, True)
         self.tableWidget.setRowCount(15)
-        self.tableWidget.setHorizontalHeaderLabels(['Serial Number', 'Protocol', 'Keys', 'Values', 'Thread', 'Status'])
+        self.tableWidget.setHorizontalHeaderLabels(['SN', 'Protocol', 'Interval', 'Status', 'Keys', 'Values', 'Value Types', 'Thread'])
 
         header = self.tableWidget.horizontalHeader()
         header.setSectionResizeMode(QHeaderView.ResizeToContents)       
@@ -135,170 +135,273 @@ class MainWindow(QMainWindow):
         self.tableWidget.customContextMenuRequested.connect(self.generateMenu)
         self.tableWidget.viewport().installEventFilter(self)
 
-        self.display_devices()
-        
+        self.tableWidget.doubleClicked.connect(self.tableWidget_doubleClicked)
+
         vbox.addWidget(self.tableWidget)
         self.deviceListBox.setLayout(vbox)
 
-       
-    def device_log_box(self):
-        self.deviceLogBox = QGroupBox("<Device Log>")
-        vbox = QVBoxLayout()
-        
-        self.list_widget = QListWidget(self)
-        self.setStyleSheet( """QListWidget{
-                color: gray;
-            }
-            """
-        )
-        self.list_widget.setEnabled(False)
-  
-        self.show_logs()
-
-        vbox.addWidget(self.list_widget)
-        self.deviceLogBox.setLayout(vbox)
-
-    @pyqtSlot()
-    def on_click(self):
-        print("\n")
-        for currentQTableWidgetItem in self.tableWidget.selectedItems():
-            print(currentQTableWidgetItem.text())
-            self.show_device_logs("sn")
+        self.display_devices()
 
     def eventFilter(self, source, event):
-        if(event.type() == QEvent.MouseButtonPress and
-            event.buttons() == Qt.RightButton and
-            source is self.tableWidget.viewport()):
+        if event.type() == QEvent.MouseButtonPress:
+            if event.button() == Qt.RightButton and source is self.tableWidget.viewport():
                 item = self.tableWidget.itemAt(event.pos())
+                self.menu = QMenu(self)
                 if item is not None:
-                    self.menu = QMenu(self)
-                    if(int(item.column()) == 0):
+                    if int(item.column()) == 0:
                         self.current_deviceSN = item.text()
                         self.startAction = self.menu.addAction('Start') 
-                        self.deleteAction = self.menu.addAction('Delete')
-                        self.stopAction = self.menu.addAction('Stop') 
-                        
+                        self.stopAction = self.menu.addAction('Stop')
+                        self.deleteAction = self.menu.addAction('Delete') 
         return super(MainWindow, self).eventFilter(source, event)
 
     def generateMenu(self, pos):
         action = self.menu.exec_(self.tableWidget.mapToGlobal(pos))
-        if(action != None):
-            if(action == self.startAction):
+        if action != None:
+            if action == self.startAction:
                 self.startDevice()
-            elif(action == self.deleteAction):
+            elif action == self.deleteAction:
                 self.deleteDevice()
-            elif(action == self.stopAction):
+            elif action == self.stopAction:
                 self.stopDevice()
             self.display_devices()
-        else:
-            print("none")
+
+    def device_log_box(self):
+        self.deviceLogBox = QGroupBox("<Device Log>")
+        vbox = QVBoxLayout()
+        
+
+        self.log_area = QPlainTextEdit(self)
+        self.log_area.setReadOnly(True)
+
+        self.log_area.setStyleSheet( """QPlainTextEdit{
+                color: white;
+                background: black;
+            }
+            """
+        )
+        
+        self.log_area.insertPlainText(f'Double click on SN column to see the running device log')
+
+        vbox.addWidget(self.log_area)
+        self.deviceLogBox.setLayout(vbox)
     
-    def startDevice(self): #before starting a device, json file can be modified to edit a device
-        if(self.check_device_status() == False):
-            deviceSN = self.current_deviceSN
-            deviceObj = Helper.get_device_data(deviceSN)
+    def tableWidget_doubleClicked(self):
+        self.stop_display_log_thread()
+
+        row = self.tableWidget.currentIndex().row()
+        sn = self.tableWidget.item(row, 0).text() #first column <SN> only
+        status = self.tableWidget.item(row, 7).text() #first column <SN> only
+
+        self.log_area.clear() # clear list items
+        self.log_area.insertPlainText(f'Device logs: {sn}\n************************\n')
+        self.get_device_logs(sn, bool(int(status)))
+
+    def get_device_logs(self, sn, status):
+        self.logger.debug(f'Show device logs: {sn}')
+        
+        logfile = Helper.get_device_log_file(sn)
+        log_lines = Helper.read_log_file(logfile)
+
+        if log_lines != None:
+            self.log_area.appendPlainText("\n".join(log_lines))
+
+            if status != False : # if the device running, start threading to display logs
+                logfilename = Helper.get_device_log_file(sn)
+                logfile = open(logfilename,"r")
+                logfile.seek(0, os.SEEK_END)
+                self.display_logs = True
+                self.display_log_thread = threading.Thread(
+                    target = self.follow_last_line, 
+                    args = (logfile,), 
+                    name = "thread-logger_"+sn
+                )
+                self.display_log_thread.setDaemon(True) 
+                self.display_log_thread.start()
+        else:
+            err = f'Log file does not exists: [{logfilename}]'
+            self.b.insertPlainText(err + "\n")
+            self.logger.debug(err)
+    
+    def follow_last_line(self, logfile):
+        while self.display_logs:
+            line = logfile.readline()
+            if line:
+                self.log_area.appendPlainText(line)
+            time.sleep(1)
+
+    def stop_display_log_thread(self):
+        if self.display_log_thread != None:
+            self.display_logs = False
+            self.display_log_thread.join()
+
+    def display_devices(self):
+        devices = Helper.read_json()['devices']
+        self.tableWidget.setRowCount(len(devices))
+        
+        
+        for i in range(len(devices)):
+            self.tableWidget.setItem(
+                i, 0, 
+                QTableWidgetItem(devices[i]['serialNumber'])
+            )
+            self.tableWidget.setItem(
+                i, 1, 
+                QTableWidgetItem(devices[i]['protocol'])
+            )
+            self.tableWidget.setItem(
+                i, 2, 
+                QTableWidgetItem(f'({devices[i]["interval"]})')
+            )
+            
+            obj = Helper.get_device_instance(self.device_instance_list, devices[i]['serialNumber'])
+            self.tableWidget.setItem(
+                i, 3, 
+                QTableWidgetItem('Stopped')
+            )
+            self.tableWidget.setItem(
+                i, 7, 
+                QTableWidgetItem('0')
+            )
+            if obj != False and obj.check_thread() != False:
+                self.tableWidget.setItem(
+                    i, 3, 
+                    QTableWidgetItem('Running')
+                )
+                self.tableWidget.setItem(
+                    i, 7, 
+                    QTableWidgetItem('1')
+                )
+            
+            #keys = [item['key'] for item in dev['keyValue']]
+            keys, init_values, value_types  = [], [], []
+            for item in devices[i]['keyValue']:
+                keys.append(item['key'])
+                init_values.append( item['initValue'] )
+                value_types.append( item['valueType'] )
+            
+            self.tableWidget.setItem(
+                i, 4, 
+                QTableWidgetItem(str(keys) )
+            )
+            self.tableWidget.setItem(
+                i, 5, 
+                QTableWidgetItem(str(init_values))
+            )
+            self.tableWidget.setItem(
+                i, 6, 
+                QTableWidgetItem(str(value_types))
+            )
+            
+        msg = f'Number of running devices: [{str(Helper.get_running_device_count())}]'
+        self.deviceCount.setText( msg )
+        self.logger.debug( msg )
+
+    def startDevice(self):
+        if self.check_device_status() == False:
+            deviceObj = Helper.get_device_data(self.current_deviceSN)
             msg = Helper.create_message(deviceObj)
             
             new_client = Client()
-            device_instance = new_client.run(deviceSN, deviceObj, msg, deviceObj['protocol'])
-            if(device_instance != None):
+            device_instance = new_client.run(self.current_deviceSN, deviceObj, msg, deviceObj['protocol'])
+            if device_instance != None:
                 self.appendToList(device_instance)
+                #self.logger.debug(f'Start device: [{self.current_deviceSN}]')
+                GUIHelper.show_message_box(
+                    self, 
+                    msg=f'Start device: [{self.current_deviceSN}]', 
+                    title='Success!'
+                )
             else:
-                print(f'Protocol is not supported! | {deviceSN} {protocol}')
-                #logging.error(f'Protocol is not supported! {deviceSN} {protocol}')
-
+                #self.logger.warning(f'Protocol is not supported!: [{self.current_deviceSN} - {protocol}]')
+                GUIHelper.show_message_box(
+                    self, 
+                    msg=f'Protocol is not supported!: [{self.current_deviceSN} - {protocol}]', 
+                    title='Warning!',
+                    msgType='warning'
+                )
         else:
-            self.showDialog('Device is already running!')
-
+            GUIHelper.show_message_box(
+                self, 
+                msg=f'The device is already running [{self.current_deviceSN}]', 
+                title='Warning!', 
+                msgType='warning'
+            )
+    
     def stopDevice(self):
-        if(self.check_device_status() == False):
-            self.showDialog('Device is already stopped!')
+        if self.check_device_status() == False:
+            GUIHelper.show_message_box(
+                self, 
+                msg=f'The device is already stopped! [{self.current_deviceSN}]', 
+                title='Warning!', 
+                msgType='warning'
+            )
         else:
-            
             deviceObj = Helper.get_device_instance(self.device_instance_list, self.current_deviceSN)
             deviceObj.stop_thread()
             self.removeFromList(deviceObj)
-            print("stop device " + self.current_deviceSN)
-            #logging.debug("stop device " + self.current_deviceSN)
+            #self.logger.debug(f'Stop device: {self.current_deviceSN}')
+            GUIHelper.show_message_box(
+                self, 
+                msg=f'Stop device: {self.current_deviceSN}', 
+                title='Success!'
+            )
 
     def deleteDevice(self):
-        if(self.check_device_status() == False):
+        if self.check_device_status() == False:
             Helper.delete_json(self.current_deviceSN)
-            #logging.debug("delete device " + self.current_deviceSN)
-            print("delete device " + self.current_deviceSN)
+            #self.logger.debug(f'Delete device: [{self.current_deviceSN}]')
+            GUIHelper.show_message_box(
+                self, 
+                msg=f'Delete device: [{self.current_deviceSN}]', 
+                title='Success!'
+            )
         else:
-            self.showDialog('Device is running!') 
-
-        
-    def display_devices(self):
-        print("display devices")
-        self.deviceCount.setText('Number of running devices: ' + str(Helper.getRunningDeviceCount()))
-        devices = Helper.read_json()['devices']
-        self.tableWidget.setRowCount(len(devices))
-        i=0
-        for dev in devices:
-            deviceSN = dev['serialNumber']
-            self.tableWidget.setItem(i, 0, QTableWidgetItem(deviceSN))
-            self.tableWidget.setItem(i, 1, QTableWidgetItem(dev['protocol']))
-            self.tableWidget.setItem(i, 2, QTableWidgetItem(f'[{dev["keyValue"][0]["key"]} {dev["keyValue"][1]["key"]} {dev["keyValue"][2]["key"]}]'))
-            self.tableWidget.setItem(i, 3, QTableWidgetItem(f'[{dev["keyValue"][0]["initValue"]} {dev["keyValue"][1]["initValue"]} {dev["keyValue"][2]["initValue"]}]'))
-            self.tableWidget.setItem(i, 4, QTableWidgetItem(f'{dev["thread"]}({dev["interval"]})'))
-
-            obj = Helper.get_device_instance(self.device_instance_list, deviceSN)
-            if(obj == False):
-                self.tableWidget.setItem(i, 5, QTableWidgetItem("Stopped"))
-            else:
-                if(obj.check_thread() != False):
-                    self.tableWidget.setItem(i, 5, QTableWidgetItem("Running"))
-                else:
-                    self.tableWidget.setItem(i, 5, QTableWidgetItem("Stopped"))
-            i+=1
-        
-    def show_logs(self):
-        self.list_widget.addItem(QListWidgetItem("0"))
-
-    def show_device_logs(self, sn):
-        print("device logs for " + sn)
-
-        #logging
-
-    def showDialog(self, msg):
-        button = QMessageBox.information(
-            self,
-            "Info!",
-            msg
-        )
+            GUIHelper.show_message_box(
+                self, 
+                msg=f'Running device cannot be deleted! [{self.current_deviceSN}]', 
+                title='Warning!', 
+                msgType='warning'
+            )
 
     def check_device_status(self):
         return Helper.get_device_instance(self.device_instance_list, self.current_deviceSN)
-
     
     def addDeviceBtn(self):
         if self.add_window.isVisible():
-            print("already visible")
+            self.logger.warning(f'AddDialog is already visible')
         else:
-            self.add_window = AddWindow(self)
+            self.logger.debug(f'AddDialog opened')
+            self.add_window = AddDialog(self)
+            self.stop_display_log_thread()
             self.add_window.show()
 
     def closeEvent(self, event):
-        quit_msg = "Are you sure you want to exit the program?"
-        reply = QMessageBox.question(self, 'Message', 
-                        quit_msg, QMessageBox.Yes, QMessageBox.No)
+        quit_msg = 'Are you sure you want to exit the program?'
+        msgBox = GUIHelper.show_message_box(
+            self, 
+            msg='Are you sure you want to exit the program?', 
+            title='Confirmation', 
+            msgType='question'
+        )
+        reply = msgBox.exec()
 
-        if reply == QMessageBox.Yes:
-            sys.exit()
+        if reply == QMessageBox.Ok:
+            #Helper.remove_device_log_files() # delete all device .log files
+            self.logger.debug(f'Delete all device log files ["./logs/deviceLogs/*"]')
+            self.logger.debug(f'Main Window closed')
+
             event.accept()
+            sys.exit()
         else:
             event.ignore()
 
     #@staticmethod
     def appendToList(self, obj):
         self.device_instance_list.append( obj )
-        self.display_devices()
     def removeFromList(self, obj):
         self.device_instance_list.remove( obj )
         
-
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)

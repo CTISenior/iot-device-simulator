62d6e0a87f73a30035e00f2a50239d941c8b49f4
Murat Alkan <muratalkanw@hotmail.com>
2022-03-18 12:12:17 +0300

    Update data generator functions

--- iotwin_data_generator/utils/data_generator.py
+++ iotwin_data_generator/utils/data_generator.py
@@ -17,7 +17,8 @@ class DataGenerator:
     def generate_data(self):
         newValue = lambda generated_value: self.value + generated_value
         self.value = newValue( self.get_value() )
-        return self.value
+        #self.value += self.get_value()
+        return self.convert(self.value)
 
     def get_value(self):
         if self.value_type == 'RN': #RandomNumber [-1, 0, 1]
@@ -31,42 +32,40 @@ class DataGenerator:
         elif self.value_type == 'RFN-4': #RandomFloatNumber [-0.01, 0, 0.01]
             return self.generate_random_float_value4()
         else: #ConstantNumber
-            return self.generate_constant()
+            return 0
 
     def generate_random_value(self):
-        return int( 
-            self.generate([-1, 0, 1]) 
-            )
-        
+        return self.generate([-1, 0, 1]) 
     def generate_random_float_value(self):
-        return float("{0:.2f}".format( 
-            self.generate([-0.1, 0, 0.1]) 
-            ))
-
+        return self.generate([-0.5, 0, 0.5])
     def generate_random_float_value2(self):
-        return float("{0:.2f}".format( 
-            self.generate([-0.5, 0, 0.5]) 
-            ))
-
+        return self.generate([-0.1, 0, 0.1])
     def generate_random_float_value3(self):
-        return float("{0:.3f}".format( 
-            self.generate([-0.05, 0, 0.05]) 
-            ))
-
+        return self.generate([-0.05, 0, 0.05])
     def generate_random_float_value4(self):
-        return float("{0:.3f}".format( 
-            self.generate([-0.01, 0, 0.01]) 
-            ))
+        return self.generate([-0.01, 0, 0.01])
 
-    def generate_constant(self):
-        return self.value
+    def convert(self, value):
+        if self.value_type == 'RN':
+            return int( value )
+        elif self.value_type == 'RFN':
+            return float("{0:.2f}".format( value ) )
+        elif self.value_type == 'RFN-2': 
+            return float("{0:.2f}".format( value ))
+        elif self.value_type == 'RFN-3':
+            return float("{0:.3f}".format( value ))
+        elif self.value_type == 'RFN-4':
+            return float("{0:.3f}".format( value ))
+        else: #ConstantNumber
+            return value
 
-    def generate(self, valueList, p=[0.05, 0.9, 0.05]):
+
+    def generate(self, valueList, p=[0.025, 0.95, 0.025]):
         return np.random.choice(valueList, 1, p)[0]
         #numpy.random.choice(numpy.arange(1, 7), p=[0.1, 0.05, 0.05, 0.2, 0.4, 0.2])
 
-    #def get_custom_value(self):
 
+    #def get_custom_value(self):
 
     #def get_prob():
 

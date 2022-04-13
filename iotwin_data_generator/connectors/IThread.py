d4acd4cf33bf090631ff2fe520fb977ef077019c
Murat Alkan <muratalkanw@hotmail.com>
2022-04-13 21:41:25 +0300

    Move interface file

---
+++ iotwin_data_generator/connectors/IThread.py
@@ -0,0 +1,10 @@
+
+
+class IThread:
+    """Thread Interface"""
+    def start(self, client_id) :
+        """start thread """
+        pass
+    def stop(self, client_id) :
+        """stop thread """
+        pass
